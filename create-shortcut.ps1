80c57d45723d4d26305e6fe613a036418c5259dc
naenergie-arch <naenergie@gmail.com>
2026-04-04 18:48:46 +0200

    Auto-sync: 2026-04-04 18:48:46

--- create-shortcut.ps1
+++
@@ -1,8 +0,0 @@
-$ws = New-Object -ComObject WScript.Shell
-$desktop = [Environment]::GetFolderPath('Desktop')
-$shortcut = $ws.CreateShortcut("$desktop\OEES Sync.lnk")
-$shortcut.TargetPath = "D:\0000000000000A-OEES_SYSTEM\OEES\START-SYNC.bat"
-$shortcut.IconLocation = "shell32.dll,77"
-$shortcut.Description = "OEES Git Auto-Sync"
-$shortcut.Save()
-Write-Host "Shortcut created on desktop"
